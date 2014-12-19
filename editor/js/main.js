37a3d82688e3fb05b3a699826dd32e27d142600f
Stuart Lee <stuart.lee@drpgroup.com>
2014-12-19 11:19:50 +0000

    added keyboard controls and tied properties panel to sprite

--- editor/js/main.js
+++ editor/js/main.js
@@ -1,3 +1,41 @@
+/**
+ * set stuff up
+ */
+PIXI.scaleModes.DEFAULT = PIXI.scaleModes.NEAREST;
+var stage = new PIXI.Stage(0xFFFFFF);
+
+// fullscreen
+var width = window.innerWidth;
+var height = window.innerHeight;
+var renderer = PIXI.autoDetectRenderer(width, height);
+
+var canvas = new PIXI.DisplayObjectContainer();
+stage.addChild(canvas);
+
+$('.stage').append(renderer.view);
+
+// global stuff to track objects
+var selected = null;
+var dragging = null;
+var dragOffset = new PIXI.Point();;
+var scale = 3;
+
+canvas.scale.set(scale);
+
+// cache a load of elements
+var elements = {
+    grid : $('.panel-browser .image-grid'),
+    position : {
+        x : $('[data-property="position-x"]'),
+        y : $('[data-property="position-y"]')
+    },
+    rotation : $('[data-property="rotation"]'),
+    scale : {
+        x : $('[data-property="scale-x"]'),
+        y : $('[data-property="scale-y"]')
+    }
+};
+
 var tree = [
     'Simple root node',
     {
@@ -22,18 +60,22 @@ $('.scene-graph').jstree({
     }
 });
 
-var grid = $('.panel-browser .image-grid');
-
+/**
+ * build the image grid
+ */
 data.images.forEach(function(image) {
 
     var item = $('<img>');
     item.attr('src', '../img/' + image);
     item.attr('draggable', false);
 
-    grid.append(item);
+    elements.grid.append(item);
 });
 
-grid.on('mousedown', 'img', function(e) {
+/**
+ * handle clicks on images in the grid
+ */
+elements.grid.on('mousedown', 'img', function(e) {
 
     var x = e.originalEvent.clientX;
     var y = e.originalEvent.clientY;
@@ -47,10 +89,12 @@ grid.on('mousedown', 'img', function(e) {
     var sprite = new PIXI.Sprite.fromImage(img);
 
     sprite.position.set(x,y);
-
+    sprite.anchor.set(0.5, 0.5);
     sprite.interactive = true;
+
     sprite.mousedown = function(e){
         dragging = sprite;
+        select(sprite);
         var pos = e.getLocalPosition(sprite);
         dragOffset.set(
             pos.x * scale,
@@ -61,47 +105,75 @@ grid.on('mousedown', 'img', function(e) {
     canvas.addChild(sprite);
 
     dragging = sprite;
-
+    select(sprite);
 });
 
-
-var selected = null;
-
-
+/**
+ * select a sprite
+ */
 function select(sprite)
 {
-
+    selected = sprite;
+    updateProperties(sprite);
 }
 
-PIXI.scaleModes.DEFAULT = PIXI.scaleModes.NEAREST;
-var stage = new PIXI.Stage(0xFFFFFF);
-var width = window.innerWidth;
-var height = window.innerHeight;
-var renderer = PIXI.autoDetectRenderer(width, height);
-var canvas = new PIXI.DisplayObjectContainer();
-stage.addChild(canvas);
-$('.stage').append(renderer.view);
+/**
+ * move sprites around with arrow keys
+ */
+$(document).on('keydown', function(e) {
 
-var dragging = null;
-var dragOffset = new PIXI.Point();;
-var scale = 3;
+    var code = e.keyCode;
 
-canvas.scale.set(scale);
+    if(selected) {
+        var update = false;
+        switch(code){
+            case Input.KEY_ARROW_UP:
+                selected.position.y -= 1;
+                update = true;
+                break;
+            case Input.KEY_ARROW_DOWN:
+                selected.position.y += 1;
+                update = true;
+                break;
+            case Input.KEY_ARROW_LEFT:
+                selected.position.x -= 1;
+                update = true;
+                break;
+            case Input.KEY_ARROW_RIGHT:
+                selected.position.x += 1;
+                update = true;
+                break;
+        }
 
+        if(update) {
+            updateProperties(selected);
+        }
+    }
+});
+
+/**
+ * handle dragging stuff
+ */
 $(document).on('mousemove', function(e) {
 
-    var x = e.originalEvent.clientX;
-    var y = e.originalEvent.clientY;
-
     if(dragging) {
+
+        var x = e.originalEvent.clientX;
+        var y = e.originalEvent.clientY;
+
         dragging.position.set(
             (x - dragOffset.x) / scale,
             (y - dragOffset.y) / scale
         );
+
+        updateProperties(dragging);
     }
 
 });
 
+/**
+ * end of drag
+ */
 $(document).on('mouseup', function(e) {
 
     if(dragging){
@@ -111,7 +183,58 @@ $(document).on('mouseup', function(e) {
 
 });
 
+/**
+ * update selected sprite when properties change
+ */
+$('.panel-properties input').on('change', function(e){
 
+    if(!selected) { return; }
+
+    var property = $(this).data('property');
+    var value = $(this).val();
+
+    switch(property) {
+        case 'position-x':
+            selected.position.x = value;
+            break;
+        case 'position-y':
+            selected.position.y = value;
+            break;
+        case 'rotation':
+            selected.rotation = value;
+            break;
+        case 'scale-x':
+            selected.scale.x = value;
+            break;
+        case 'scale-y':
+            selected.scale.y = value;
+            break;
+    }
+
+});
+
+/**
+ * stop keyboard events in the input fields triggering the main handler
+ */
+$('.panel-properties input').on('keydown', function(e) {
+    e.stopPropagation();
+});
+
+/**
+ * update a sprite from the properties window
+ */
+function updateProperties(sprite)
+{
+    elements.position.x.val( sprite.position.x );
+    elements.position.y.val( sprite.position.y );
+    elements.rotation.val( sprite.rotation );
+    elements.scale.x.val( sprite.scale.x );
+    elements.scale.y.val( sprite.scale.y );
+}
+
+/**
+ * render the main canvas
+ */
 function render()
 {
     requestAnimationFrame(render);
